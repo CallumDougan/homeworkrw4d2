7914e4d332a230bd988b9588bbc6340a2b92a140
CallumDougan <practicalitymalfunction@gmail.com>
2015-12-08 18:09:53 +0000

    implemented show

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,11 +5,25 @@ class CountriesController < ApplicationController
   end
 
   def new
-
+    @country = Country.new
   end
 
   def show
-    
+    country_finder
+  end
+
+  def create
+    Country.create(country_params)
+    redirect_to(countries_path)
+  end
+
+  private
+  def country_params
+    params.require(:country).permit(:name, :flag, :size, :population, :language, :travel_advice)
+  end
+
+  def country_finder
+    @country = Country.find(params[:id])
   end
 
 end
